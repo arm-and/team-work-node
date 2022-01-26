fcf6bfa6e88cab8bb62d9073559e68fa93db165f
Andrii Zhabynets <a.zhabynets03@gmail.com>
2022-01-26 21:15:32 +0200

    connect to the server commit

---
+++ Script.js
@@ -0,0 +1,16 @@
+
+async function getResponse(){
+    let response = await fetch('http://localhost:3000/')
+    let content = await response.json()
+
+let list = document.querySelector('.posts')
+    let key;
+        for (key in content){
+            list.innerHTML += `
+            <h2>User id: ${content[key].id}</h2>
+            <p>Name: ${content[key].first_name}<p>
+            <p>Lastname: ${content[key].last_name}<p>
+            <p>Email: ${content[key].email}<p>
+            <img src="${content[key].photo}">`
+        }
+}
